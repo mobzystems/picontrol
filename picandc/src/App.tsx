88fbd2717b5af48a14d16ff823bd657346261806
Markus The <markus@mobzystems.nl>
2024-03-28 13:16:08 +0100

    Bulma and grid layout

--- picandc/src/App.tsx
+++ picandc/src/App.tsx
@@ -7,21 +7,50 @@ interface RunResult {
   exception: string; // '' is OK
 }
 
-function App() {
+export default function App() {
   const [scripts, setScripts] = useState<string[]>();
+
+  useEffect(() => {
+    if (scripts === undefined) {
+      fetch(`/run/`)
+        .then(r => r.json())
+        .then((data: string[]) => setScripts(data))
+        .catch(error => console.log(error))
+        ;
+    }
+  }, []);
+
+  return (<>
+    <div className="app">
+      {scripts === undefined ?
+        <div id="content"><p>Loading...</p></div>
+        :
+        <Main scripts={scripts}></Main>
+      }
+    </div>
+  </>);
+}
+
+function Main(props: {
+  scripts: string[]
+}) {
   const [currentScript, setCurrentScript] = useState('');
+  const [currentTab, setCurrentTab] = useState<'Output' | 'Error' | 'Exception'>('Output');
+
   const [state, setState] = useState<{ running: boolean, result: RunResult | undefined }>({ running: false, result: undefined });
 
   async function fetchData(script: string) {
     setState({ running: true, result: undefined });
+    setCurrentTab('Output');
+
     try {
-    var r = await fetch(`/run/${script}`);
-    if (!r.ok) {
-      console.log(r.status); 
-      setState({ running: false, result: { output: "", error: "", exception: `Server returned status ${r.status}` }});
-    } else {
-      var data = await r.json() as RunResult;
-      setState({ running: false, result: data });
+      var r = await fetch(`/run/${script}`);
+      if (!r.ok) {
+        console.log(r.status);
+        setState({ running: false, result: { output: "", error: "", exception: `Server returned status ${r.status}` } });
+      } else {
+        var data = await r.json() as RunResult;
+        setState({ running: false, result: data });
       }
     } catch (error: any) {
       console.log("Error occurred " + error);
@@ -29,53 +58,91 @@ function App() {
     }
   }
 
-  useEffect(() => {
-    if (scripts === undefined) {
-      fetch(`/run/`)
-      .then(r => r.json())
-      .then((data: string[]) => setScripts(data))
-      .catch(error => console.log(error))
-      ;
-    }
-  }, []);
-
-  if (scripts === undefined)
-    return (<p>Loading...</p>);
-
   return (
     <>
-      <div>
-        <select disabled={state.running} value={currentScript} onChange={e => setCurrentScript(e.target.value)}>
-          <option value={''}>Choose a script</option>
-          {scripts.map(s => <option key={s} value={s}>{s}</option>)}
-        </select>
-        <button disabled={state.running || currentScript === ''} onClick={() => fetchData(currentScript!)}>Run</button>
-        <button disabled={state.running} onClick={() => setState({ running: false, result: undefined })}>Clear</button>
+      <div id="top">
+        <div className="field has-addons">
+          <p className="control is-expanded">
+            <span className="select is-fullwidth">
+              <select disabled={state.running} value={currentScript} onChange={e => setCurrentScript(e.target.value)}>
+                <option value={''}>Choose a script</option>
+                {props.scripts.map(s => <option key={s} value={s}>{s}</option>)}
+              </select>
+            </span>
+          </p>
+          <p className="control">
+            <button className={`button is-success${state.running ? ' is-loading' : ''}`} disabled={state.running || currentScript === ''} onClick={() => fetchData(currentScript!)}>Run</button>
+          </p>
+          <p className="control">
+            <button className="button is-danger" disabled={state.running} onClick={() => setState({ running: false, result: undefined })}>Clear</button>
+          </p>
+        </div>
       </div>
-      {state.running
-        ?
-        <p>Running...</p>
-        : <>
-          {state.result === undefined ?
-            <p>Press the button already!</p>
-            :
-            <>
-              <p>
-                Result: {state.result.exception === '' ? 'The command completed successfully.' : state.result.exception}
-              </p>
-              <p>Output:</p>
-              <pre className="output">
-                {state.result.output === '' ? "(No output)" : state.result.output}
-              </pre>
-              {state.result.error !== '' && <>
-                <p>Error:</p>
-                <pre className="error">{state.result.error}</pre>
-              </>}
-            </>
-          }
-        </>}
+
+      <div id="content">
+        {state.running
+          ?
+          <p>Running...</p>
+          : <>
+            {state.result === undefined ?
+              <p>Please select a command.</p>
+              :
+              <>
+              <div id="messages">
+                {state.result.exception !== '' &&
+                  <article className="message is-danger">
+                    {/* <div className="message-header">There was an exception</div> */}
+                    <div className="message-body">{state.result.exception}</div>
+                  </article>
+                }
+
+                {state.result.error !== '' &&
+                  <article className="message is-warning">
+                    {/* <div className="message-header">There was an error output</div> */}
+                    <div className="message-body">See the Error tab for errors</div>
+                  </article>
+                }
+                </div>
+                
+                <div className="tabs">
+                  <ul>
+                    <li onClick={() => setCurrentTab('Output')} className={currentTab === 'Output' ? 'is-active' : ''}><a>Output</a></li>
+                    <li onClick={() => setCurrentTab('Error')} className={currentTab === 'Error' ? 'is-active' : ''}><a>Error</a></li>
+                    {/* <li onClick={() => setCurrentTab('Exception')} className={currentTab === 'Exception' ? 'is-active' : ''}><a>Exception</a></li> */}
+                  </ul>
+                </div>
+
+                <div className="scroller">
+
+                  {currentTab === 'Output' && <>
+                    {state.result.output === '' ?
+                      <p>There was no output.</p>
+                      : <pre className="">
+                        {state.result.output === '' ? "(No output)" : state.result.output}
+                      </pre>
+                    }
+                  </>
+                  }
+
+                  {currentTab === 'Error' && <>
+                    {state.result.error === '' ?
+                      <p>There were no errors.</p>
+                      :
+                      <pre className="has-text-danger">{state.result.error}</pre>
+                    }
+                  </>
+                  }
+                  {/* {currentTab === 'Exception' &&
+                  <>
+                    {state.result.exception === '' ? <p>The command completed uccessfuly</p> : <p>{state.result.exception}</p>}
+                  </>
+                } */}
+
+                </div>
+              </>
+            }
+          </>}
+      </div>
     </>
   )
 }
-
-export default App
